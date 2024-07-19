76a4818485f755c92295dd396cb153f78ced5c92
ImranBecirovic <imran.becirovic11@gmail.com>
2024-07-20 00:33:16 +0200

    qqq

--- src/Components/sideComponents/countryStats.jsx
+++ src/Components/sideComponents/countryStats.jsx
@@ -1,13 +1,32 @@
 import React from "react";
-
-const CountryStats = () => {
+function CountryStats() {
     return (
-        <>
-            <div className="countryStats">
-                <p>Country Stats</p>
-            </div>
-        </>
-    );
-};
+      <>
+        <div className="container"> 
+          <div className="menus"></div>
+          <div className="country-info">
+            {/* ispod su po blokovima postavljene informacije o drzavama */}
+            <div className="country-name"></div>
 
-export default CountryStats;
+            <div>
+                <div className="country-new-cases">
+FDSFDDSFSFDDSFSFD
+                </div>
+                <div className="country-new-deaths">
+
+                </div>
+                <div className="country-new-recovered">
+
+                </div>
+            </div>
+
+            <div className="country-total-active">
+
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+  
+  export default CountryStats;
